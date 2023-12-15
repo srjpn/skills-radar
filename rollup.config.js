7664add22046dec0eb8877c0c1316e8ceae18abb
Sooraj Parameswaran <srjpn123@gmail.com>
2023-12-15 14:56:41 +0530

    Added cjs and esm modules

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,31 @@ const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const typescript = require("@rollup/plugin-typescript");
 const postcss = require("rollup-plugin-postcss");
+const { dts } = require("rollup-plugin-dts");
+const packageJson = require("./package.json");
+
 
 module.exports = [
   {
     input: "src/index.ts",
     output: [
       {
-        file: "dist/index.js",
+        file: "dist/umd/index.js",
         format: "umd",
         sourcemap: true,
-        name: 'SkillRadar',
+        name: 'skills-radar',
+      },
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+        name: 'skills-radar',
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+        name: 'skills-radar',
       },
     ],
     plugins: [
@@ -20,5 +35,11 @@ module.exports = [
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
     ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Radar } from "./Radar";
+export { Blips as Blips } from "./Radar/Blips";
