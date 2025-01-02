2050fa1ed87399daa3e6bbcaa8c4de0fd7e6842d
Sooraj Parameswaran <srjpn123@gmail.com>
2025-01-02 18:32:10 +0530

    Pulled out proficiencyLevels to a common place

---
+++ src/data/proficiencyLevels.ts
@@ -0,0 +1,18 @@
+export const proficiencyLevels = [
+    {
+        title: "Expert",
+        description: "Skills in which I have achieved a high level of proficiency and confidence. These are my go-to abilities for solving complex problems, and I leverage them effectively in real-world scenarios. I continuously refine and expand these skills, often mentoring others or setting best practices in these areas."
+    },
+    {
+        title: "Growth",
+        description: "Skills I am actively learning or refining. I am exploring these through projects, courses, or other deliberate efforts. I see strong potential in these areas to become part of my 'Expert' ring with further practice and exposure."
+    },
+    {
+        title: "Explore",
+        description: "Skills I'm interested in and eager to learn. These are areas I find valuable or exciting and plan to invest time in developing further."
+    },
+    {
+        title: "Pause",
+        description: "Skills I've decided to set aside for now. They may have been useful in the past but are no longer a focus due to lack of interest, changes in career direction, or reduced demand. I avoid investing additional time in these unless a strong reason emerges."
+    }
+];
